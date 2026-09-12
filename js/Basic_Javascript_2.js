35f32d5abdea823f2b69c53cfc3105643cf6c9a0
Deivid <deividmattos79@hotmail.com>
2026-09-12 15:26:02 +0100

    Add JavaScript and Irish flag background on button click

---
+++ js/Basic_Javascript_2.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+
+    let String = "Kiss me, I'm Irish";
+
+    let result = String.fontcolor("white");
+
+    document.getElementById("Irish").innerHTML = result;
+document.body.style.background = 
+    "linear-gradient(to right, green 33.33%, white 33.33% 66.66%, orange 66.66%)";
+}
